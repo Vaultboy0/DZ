a4e7a57f49372040fbe36a9e9366ae14a00f62b4
Vault_boy0 <oleg_95.95@mail.ru>
2022-11-22 00:14:44 +0300

    DZ 1

---
+++ DZ/DZ1/Program.cs
@@ -0,0 +1,75 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.WriteLine("Enter number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+if (num1>num2)
+{
+    Console.WriteLine($"{num1} > {num2}");
+    max=num1;
+    min=num2;
+}
+else
+{
+    Console.WriteLine($"{num2} > {num1}");
+    max=num2;
+    min=num1;
+}
+Console.WriteLine($"max number {max} and min number {min}");
+*/
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.WriteLine("Enter number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    if(num1>num3)
+    {
+        Console.WriteLine($"Max number {num1}");
+    }
+    else
+    {
+        Console.WriteLine($"Max number {num3}");
+    }
+}
+else
+{
+    if(num2>num3)
+    {
+        Console.WriteLine($"Max number {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"Max number {num3}");
+    }
+}
+*/
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.WriteLine("Enter number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1%2==0)
+{
+    Console.WriteLine($"Number {num1} even");
+}
+else
+{
+    Console.WriteLine($"Number {num1} not even");
+}
+*/
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*Console.WriteLine("Enter number N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Even number: ");
+for(int i=0; i<numberN;i=i+2)
+{
+    Console.Write($" {i} ");
+}
+*/
---
+++ seminar/seminar1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+int FindBiggtstDigit (int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if(ed>dec)
+    {
+        return ed;
+    }
+    else
+    {
+        return dec;
+    }
+}
+int rand = new Random().Next(10,100);
+Console.WriteLine($"Your number -> {rand}"); // Вывод нашего числа
+int result = FindBiggtstDigit(rand);
+Console.WriteLine($"The biggest Digit -> {result}"); // Вывод большего из двух
+*/
+/*
+int CutNumber(int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    int res = sot*10+ed;
+    return res;
+}
+int rand = new Random().Next(100,1000);
+int newNumber = CutNumber(rand);
+Console.WriteLine($"New version of {rand} is {newNumber}");
+*/
+
