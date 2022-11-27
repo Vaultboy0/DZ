81709ef69c46a532b46370cb58a212f1c8c9ccce
Vault_boy0 <oleg_95.95@mail.ru>
2022-11-27 18:03:36 +0300

    Забыл добавить 4 задание

--- DZ/DZ3/Program.cs
+++ DZ/DZ3/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+void Palindrom (int a)
+{
+    int number1=(a%100)/10;
+    Console.WriteLine($" {number1} ");
+    int number2=(a/1000)%10;
+    Console.WriteLine($"{number2}  ");
+    if(number1==number2) 
+    {
+        Console.WriteLine($"Число {a}  палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} не палиндром");
+    }
+}
+Console.WriteLine($"Enter the number");
+int digit1=Convert.ToInt32(Console.ReadLine());
+Palindrom(digit1);
+*/
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+void distance(double x1, double y1, double z1, double  x2, double y2, double z2)
+{
+    //double d=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+    //Console.WriteLine($"Расстояние между двумя точками а1 и а2 в 3д пространстве => {d}");
+    Console.WriteLine($"Расстояние между двумя точками а1 и а2 в 3д пространстве => {Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2))}");
+}
+Console.WriteLine($"Введите координату х для точки 1");
+double x1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите координату y для точки 1");
+double y1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите координату z для точки 1");
+double z1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите координату х для точки 2");
+double x2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите координату y для точки 2");
+double y2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введите координату z для точки 2");
+double z2=Convert.ToDouble(Console.ReadLine());
+distance(x1,y1,z1,x2,y2,z2);
+*/
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void cub(int n)
+{
+    int result;
+    Console.WriteLine($"Таблица кубов числа {n} => ");
+    for(int i=1;i<=n;i++)
+    {
+        result=i*i*i;
+        Console.Write($" {result} ");
+    }
+}
+Console.WriteLine($"Enter the number N");
+int digit1=Convert.ToInt32(Console.ReadLine());
+cub(digit1);
+*/
---
+++ DZ/DZ4/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// void pow(double a, double b)
+// {
+//     Console.WriteLine($"Числа {a} в степени {b} => {Math.Pow(a,b)}"); Вариант через функцию возведения в степень
+// }
+/*
+void pow(int a, int b)
+{
+    if(b!=0)
+    {
+    int result=a;
+    for(int i=1; i<b;i++)
+    {
+        result=result*a;
+    }
+    Console.Write($"Числа {a} в степени {b} => {result} ");
+    }
+    else
+    {
+        Console.Write($"Числа {a} в степени {b} => {1}");
+    }
+}
+Console.WriteLine($"ВВедите число которое нужно возвести в степень");
+int digit1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите степень числа");
+int digit2=Convert.ToInt32(Console.ReadLine());
+pow(digit1,digit2);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int summ(int A)
+{
+    int result=0;
+    while(A!=0)
+    {
+        result=result+A%10;
+        A=A/10;
+    }
+    return result;
+}
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int Summa=summ(a);
+Console.WriteLine($"Сумма цифр числа {a} => {Summa}");
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+   int [] newArray  = new int [size];
+   for(int i=0; i < size; i++)
+   {
+    newArray[i] = new Random().Next(minValue, maxValue+1);
+   }
+   return newArray;
+}
+ void ShowArray (int[] array)
+ {
+    Console.Write("Получившийся массив ->");
+    for(int i=0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+   // Console.WriteLine();
+ }
+ Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(a,min,max);
+ShowArray(myArray);
+*/
