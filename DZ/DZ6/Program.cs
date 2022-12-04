ec1bd15b46eb911f476361ff35aa7832b1e35ee6
Vault_boy0 <oleg_95.95@mail.ru>
2022-12-04 06:09:33 +0300

    дз вплоть до 7

---
+++ DZ/DZ6/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+ /*
+int CorrectValue() 
+{
+    int value;
+    while (!int.TryParse(Console.ReadLine(), out value)) //проверка, что введение значение число, а не символы
+    {
+        Console.WriteLine("Ошибка ввода! Введите целое число ");
+    }
+    return value;
+}
+int[] CreateArray (int size)
+{
+   int [] newArray  = new int [size];
+   for(int i=0; i < size; i++)
+   {
+    newArray[i] = CorrectValue();
+   }
+   return newArray;
+}
+ int[] ShowArray (int[] array)
+ {
+    Console.Write("Получившийся массив ->");
+    for(int i=0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+   Console.WriteLine();
+   return array;
+ }
+int CountBigZero(int[] array)       
+{
+    int count=0;
+    for(int i=0; i<array.Length;i++)
+    {
+        if(array[i]>0) count++;
+    }
+    return count;
+}
+Console.WriteLine("Сколько чисел вы будете вводить? ");
+int a = CorrectValue();
+int[] myArray = CreateArray(a);
+ShowArray(myArray);
+Console.WriteLine($"Количество числе больше нуля в данном массиве данных => {CountBigZero(myArray)}");
+*/
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+ /*
+double PointX(double k1, double b1, double k2, double b2) // поиск точки х
+{
+    return ((-b2 + b1)/(-k1 + k2));
+}
+double PointY(double X, double k2, double b2) // поиск точки y
+{
+    return (k2 * X + b2);
+}
+Console.WriteLine("Введите значение k1");
+double k1= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+// double x=PointX(k1,b1,k2,b2);
+// double y=PointY(x,k2,b2);
+// Console.WriteLine($"Точка пересечения для двух прямых  => {x} ; {y}");
+Console.WriteLine($"Точка пересечения для двух прямых =>  {PointX(k1,b1,k2,b2)} ; {PointY(PointX(k1,b1,k2,b2),k2,b2)}"); // через вывод без переменных
+*/
---
+++ DZ/DZ7/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] CreateRandomArray(int size, int size2, int minValue, int maxValue)
+{
+    int[,] newArray = new int[size, size2];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return newArray;
+}
+int[,] ShowArray(int[,] array)
+{
+    Console.WriteLine("Получившийся массив ->");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+void Research(int a, int b, int[,] array)
+{
+    if (array.Length>=(a*b))
+    {
+        Console.WriteLine($"Элемент с индексами [{a},{b}] => {array[a-1, b-1]}");
+    }
+    else
+    {
+        Console.WriteLine($"В данном массиве нету элемента с индексами [{a},{b}]");
+    }
+}
+void ArithmeticMean(int[,] array)
+{
+    Console.WriteLine("Среднее арифметическое стобцов данного массива => ");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double summ = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            summ =summ + array[j, i];
+        }
+        Console.Write($"{summ/array.GetLength(0)} ");
+    }
+}
+Console.WriteLine("Введите количество cтрок");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] myArray = CreateRandomArray(N, M, min, max);
+ShowArray(myArray);
+Console.WriteLine("Введите номер строки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Research(x1, y1, myArray);
+ArithmeticMean(myArray);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
